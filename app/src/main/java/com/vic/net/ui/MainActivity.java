f40c38e377af9601eb96bcf7e8876a3fabb832d6
桃花林里练醉拳 <jsmrliu@gmail.com>
2017-05-05 16:12:36 +0800

    error

--- app/src/main/java/com/vic/net/ui/MainActivity.java
+++ app/src/main/java/com/vic/net/ui/MainActivity.java
@@ -10,6 +10,7 @@ import com.vic.net.api.ApiService;
 import com.vic.net.vo.WeatherVo;
 import com.vic.rxnetsdk.CommonSubscriber;
 import com.vic.rxnetsdk.RxRetrofit;
+import com.vic.rxnetsdk.RxSubscriber;
 
 import rx.android.schedulers.AndroidSchedulers;
 
@@ -40,28 +41,34 @@ public class MainActivity extends RxAppCompatActivity implements View.OnClickLis
 //                .queryWeatherByScalars("上海", "c835721be56ed3b6e603c6873625d4d5")
                 .compose(this.<WeatherVo>bindToLifecycle())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new CommonSubscriber<WeatherVo>() {
-
+                .subscribe(new RxSubscriber<WeatherVo>() {
                     @Override
-                    public void onStart() {
-                        super.onStart();
-                    }
-
-                    @Override
-                    public void onCompleted() {
-//                        Toast.makeText(RetrofitActivity.this, "onCompleted", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Toast.makeText(MainActivity.this, "onError", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onNext(WeatherVo dataSet) {
-//                        Toast.makeText(MainActivity.this, ""+dataSet, Toast.LENGTH_SHORT).show();
+                    protected void callBack(WeatherVo dataSet) {
                         Toast.makeText(MainActivity.this, "onNext=天气预报时间：" + dataSet.getResult().getData().getWeather().get(0).getDate(), Toast.LENGTH_SHORT).show();
                     }
                 });
+//                .subscribe(new CommonSubscriber<WeatherVo>() {
+//
+//                    @Override
+//                    public void onStart() {
+//                        super.onStart();
+//                    }
+//
+//                    @Override
+//                    public void onCompleted() {
+////                        Toast.makeText(RetrofitActivity.this, "onCompleted", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Toast.makeText(MainActivity.this, "onError", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                    @Override
+//                    public void onNext(WeatherVo dataSet) {
+////                        Toast.makeText(MainActivity.this, ""+dataSet, Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(MainActivity.this, "onNext=天气预报时间：" + dataSet.getResult().getData().getWeather().get(0).getDate(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
     }
 }
--- rxnetsdk/src/main/java/com/vic/rxnetsdk/RxSubscriber.java
+++ rxnetsdk/src/main/java/com/vic/rxnetsdk/RxSubscriber.java
@@ -1,8 +1,62 @@
 package com.vic.rxnetsdk;
 
+import android.net.ParseException;
+import android.util.Log;
+
+import com.google.gson.JsonParseException;
+
+import org.json.JSONException;
+
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import retrofit2.HttpException;
+import rx.Subscriber;
+
 /**
  * Created by liu song on 2017/5/4.
  */
 
-public class RxSubscriber {
+public abstract class RxSubscriber<T> extends Subscriber<T> {
+
+    private static final String TAG = CommonSubscriber.class.getSimpleName();
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onCompleted()");
+    }
+
+    @Override
+    public void onCompleted() {
+        Log.i(TAG, "onCompleted()");
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        Log.i(TAG, "onError()");
+        if (e instanceof HttpException) {
+            /*HTTP错误*/
+        } else if (e instanceof ConnectException || e instanceof SocketTimeoutException) {
+            /*链接异常*/
+        } else if (e instanceof JsonParseException
+                || e instanceof JSONException
+                || e instanceof ParseException) {
+            /*解析错误*/
+        } else if (e instanceof UnknownHostException) {
+            /*解析该域名异常*/
+        } else {
+            /*未知异常*/
+        }
+    }
+
+    @Override
+    public void onNext(T t) {
+        Log.i(TAG, "onNext()");
+        callBack(t);
+    }
+
+    //成功回调
+    protected abstract void callBack(T t);
 }
